2c6e5466befd320bc82252231bb10bf9469a181f
ElianDiaz03 <87671672+ElianDiaz03@users.noreply.github.com>
2021-11-09 07:59:26 -0600

    jeje

---
+++ EjercicioParcial/EjercicioParcial/Controllers/automaticoController.cs
@@ -0,0 +1,56 @@
+﻿using EjercicioParcial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EjercicioParcial.Controllers
+{
+    public class automaticoController : Controller
+    {
+        // GET: automatico
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Indexx()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Dinero()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ErrorNumero()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Indexx(automatico p)
+        {
+            if (p.Dinero % 5 == 0)
+            {
+                return Redirect("Dinero");
+            }
+            else
+            {
+                return Redirect("ErrorNumero");
+            }
+
+
+           
+            
+
+
+        }
+       
+    }
+}
